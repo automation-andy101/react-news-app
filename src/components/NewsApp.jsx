8d627479229a72c05c13b82c58b02a68e4802706
automation.andy101 <automation.andy101@gmail.com>
2025-08-10 11:59:10 +0100

    Started adding GNews API Get new functionality

--- src/components/NewsApp.jsx
+++ src/components/NewsApp.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import techImg from '../assets/images/tech.jpg'
 import worldImg from '../assets/images/world.jpg'
 import sportsImg from '../assets/images/sports.jpg'
@@ -5,13 +6,31 @@ import scienceImg from '../assets/images/science.jpg'
 import healthImg from '../assets/images/health.jpg'
 import entertainmentImg from '../assets/images/entertainment.jpg'
 import nationImg from '../assets/images/nation.jpg'
-
+import './NewsApp.css'
+import axios from 'axios'
 
 const NewsApp = () => {
-  return (
-    <div className="news-app">
-        <div className="news-header">
-            <h1 className="logo">News App</h1>
+    const [headline, setHeadline] = useState(null)
+    const [news, setNews] = useState([])
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            const apiKey = import.meta.env.VITE_GNEWS_API_KEY;
+            const url = `https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=${apiKey}`
+            const response = await axios.get(url)
+            const fetchedNews = response.data.articles
+
+            setHeadline(fetchedNews[1])
+            setNews(fetchedNews.slice(2, 8))
+        }
+        fetchNews()
+    }, [])
+
+    return (
+        <div className="news-app">
+            <div className="news-header">
+                <h1 className="logo">News App</h1>
+            </div>
             <div className="news-content">
                 <nav className="navbar">
                     <h1 className="nav-heading">Categories</h1>
@@ -27,15 +46,37 @@ const NewsApp = () => {
                         <a href="#" className="nav-link">Nation</a>
                     </div>
                 </nav>
+
                 <div className="news-section">
-                    <div className="headline">
-                        <img src={techImg} alt="Headline Image" />
-                    </div>
+                    {headline && (
+                        <div className="headline">
+                            <img src={headline.image} alt={headline.title} />
+                            <h2 className="headline-title">{headline.title}</h2>
+                        </div>
+                    )}
+
+                    {news && (
+                        <div className="news-grid">
+                            {news.map((article, index) => (
+                                <div className="news-grid-item">
+                                    <img src={article.image} key={index} alt={article.title} />
+                                    <h3>{article.title}</h3>
+                                </div>
+                            ))}                                                                                          */}
+                        </div>
+                    )}    
                 </div>
+                
             </div>
+            <footer>
+                <p className="copyright">
+                    <span>News App</span>
+                </p>
+                <p>&copy; All Rights Reserved. By Divine Development</p>
+
+            </footer>
         </div>
-    </div>
-  )
+    )
 }
 
 export default NewsApp
